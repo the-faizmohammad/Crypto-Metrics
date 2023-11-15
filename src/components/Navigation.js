231edb9882bc7452bec34fedc4255c73bee3d0d2
user-faiz <email2faizm@gmail.com>
2023-11-15 16:55:16 +0530

    Add cryptoslice nad component structure

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Navigation = () => {
+  const iconStyle = {
+    color: 'rgb(212, 209, 209)',
+    cursor: 'pointer',
+    textDecoration: 'none',
+  };
+
+  return (
+  <h1></h1>
+  );
+};
+
+export default Navigation;
