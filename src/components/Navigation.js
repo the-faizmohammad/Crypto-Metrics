dc2e78d9aa8c19425c5fab4ea69eb05ecb54b809
user-faiz <email2faizm@gmail.com>
2023-11-15 17:57:31 +0530

    fixed linters error

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
-
-const Navigation = () => {
-  const iconStyle = {
-    color: 'rgb(212, 209, 209)',
-    cursor: 'pointer',
-    textDecoration: 'none',
-  };
-
-  return (
-  <h1></h1>
-  );
-};
+const Navigation = () => (
+  <h1>Faiz</h1>
+);
 
 export default Navigation;
